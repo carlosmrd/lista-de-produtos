f3a4f6d7142e9d860533920b93a9b17510cd80c0
Carlos <carlosmrd2001@gmail.com>
2025-05-27 02:37:08 -0300

    Reformat code

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -10,6 +10,7 @@ public class Main {
     public static void main(String[] args) {
 
         while (true) {
+
             System.out.println("\n----- MENU -----");
             System.out.println("1. Cadastro de produto");
             System.out.println("2. Listar ou buscar produtos");
